29558da7ea64851611c6f0609444c7482e64fd09
Dustin Bachrach <ahdustin@gmail.com>
2015-06-25 10:36:57 -0700

    Document MHSorting

--- Pod/Model/Container/MHSorting.h
+++ Pod/Model/Container/MHSorting.h
@@ -9,10 +9,22 @@
 #import <JSONModel/JSONModel.h>
 
 
+/**
+ * An MHSorting will appear inside of an MHContext indicating how the MHObject should be sorted
+ * relative to other objects in the results.
+ * Since there are multiple ways to sort results, MHSorting allows you to see how an object
+ * should be positioned in different types of sorting.
+ */
 @interface MHSorting : JSONModel
 
+/**
+ * The importance of this MHObject compared to other MHObjects in the result.
+ */
 @property (strong, nonatomic) NSNumber<Optional>* importance;
 
+/**
+ * The raw positional order of this MHObject compared to other MHObjects in the result.
+ */
 @property (strong, nonatomic) NSNumber<Optional>* position;
 
 @end
